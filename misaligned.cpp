c990fbd8aaf9a94df8c39202843a5fb9c6eb4b78
qei1cob <qei1cob@bosch.com>
2025-08-02 23:54:30 +0530

    [Misaligment][Colourcode]:Aligning print of code
    Fix for Aligning and printing of code

--- misaligned.cpp
+++ misaligned.cpp
@@ -1,15 +1,19 @@
 #include <iostream>
 #include <assert.h>
+#include <iomanip>
 
 int printColorMap() {
     const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
     const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
 
-    int pairNumber = 0;
+    std::cout << std::left << std::setw(7) << "PairNumber" << std::setw(10) << "Major" << std::setw(10) << "Minor" << "\n";
+    std::cout << std::string(27, '-') << "\n";
+
+    int pairNumber = 1;
     int i = 0, j = 0;
     for(int i = 0; i < 5; i++) {
         for(int j = 0; j < 5; j++) {
-            std::cout << pairNumber << " | " << majorColor[i] << " | " << minorColor[j] << "\n";
+            std::cout << std::setw(7) << pairNumber << std::setw(10) << majorColor[i] << std::setw(10) << minorColor[j] << "\n";
             ++pairNumber;
         }
     }
