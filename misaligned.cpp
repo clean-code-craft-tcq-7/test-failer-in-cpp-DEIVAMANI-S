e7b5f28b37c9f9ac0510ecf29048115b77d91e53
qei1cob <qei1cob@bosch.com>
2025-08-03 21:24:36 +0530

    CleanCoding Misalignment:Covering all test cases
    CleanCoding Misalignment:Covering all test cases with wrong test case

--- misaligned.cpp
+++ misaligned.cpp
@@ -42,8 +42,60 @@ void testGenerateColorPairs() {
     // Test case to check the possible combinations
     assert(pairs[0].first == "White");
     assert(pairs[0].second == "Blue");
+    assert(pairs[1].first == "White");
+    assert(pairs[1].second == "Orange");
+    assert(pairs[2].first == "White");
+    assert(pairs[2].second == "Green");
+    assert(pairs[3].first == "White");
+    assert(pairs[3].second == "Brown");
+    assert(pairs[4].first == "White");
+    assert(pairs[4].second == "Slate");
+
+    assert(pairs[5].first == "Red");
+    assert(pairs[5].second == "Blue");
+    assert(pairs[6].first == "Red");
+    assert(pairs[6].second == "Orange");
+    assert(pairs[7].first == "Red");
+    assert(pairs[7].second == "Green");
+    assert(pairs[8].first == "Red");
+    assert(pairs[8].second == "Brown");
+    assert(pairs[9].first == "Red");
+    assert(pairs[9].second == "Slate");
+
+    assert(pairs[10].first == "Black");
+    assert(pairs[10].second == "Blue");
+    assert(pairs[11].first == "Black");
+    assert(pairs[11].second == "Orange");
+    assert(pairs[12].first == "Black");
+    assert(pairs[12].second == "Green");
+    assert(pairs[13].first == "Black");
+    assert(pairs[13].second == "Brown");
+    assert(pairs[14].first == "Black");
+    assert(pairs[14].second == "Slate");
+
+    assert(pairs[15].first == "Yellow");
+    assert(pairs[15].second == "Blue");
+    assert(pairs[16].first == "Yellow");
+    assert(pairs[16].second == "Orange");
+    assert(pairs[17].first == "Yellow");
+    assert(pairs[17].second == "Green");
+    assert(pairs[18].first == "Yellow");
+    assert(pairs[18].second == "Brown");
+    assert(pairs[19].first == "Yellow");
+    assert(pairs[19].second == "Slate");
+
+    assert(pairs[20].first == "Violet");
+    assert(pairs[20].second == "Blue");
+    assert(pairs[21].first == "Violet");
+    assert(pairs[21].second == "Orange");
+    assert(pairs[22].first == "Violet");
+    assert(pairs[22].second == "Green");
+    assert(pairs[23].first == "Violet");
+    assert(pairs[23].second == "Brown");
+    // assert(pairs[24].first == "Violet");
+    // assert(pairs[24].second == "Slate");
     assert(pairs[24].first == "Violet");
-    assert(pairs[24].second == "Slate");
+    assert(pairs[24].second == "Yellow");
 
     std::cout << "All color pair tests passed!\n";
 }
