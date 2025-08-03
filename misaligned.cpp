1159e9b440080baf9ac35615f0cdb9869bb17fa0
qei1cob <qei1cob@bosch.com>
2025-08-03 19:38:47 +0530

    [TestFailer] ColourCode fix for alignment
    [TestFailer] ColourCode fix for alignment

--- misaligned.cpp
+++ misaligned.cpp
@@ -1,3 +1,4 @@
+what will be the output of below c++ program
 #include <iostream>
 #include <assert.h>
 #include <iomanip>
@@ -6,14 +7,14 @@ int printColorMap() {
     const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
     const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
 
-    std::cout << std::left << std::setw(7) << "PairNumber" << std::setw(10) << "Major" << std::setw(10) << "Minor" << "\n";
+    std::cout << std::left << std::setw(12) << "Pair No.," << std::setw(10) << "Major" << std::setw(10) << "Minor" << "\n";
     std::cout << std::string(27, '-') << "\n";
 
     int pairNumber = 0;
     int i = 0, j = 0;
     for(int i = 0; i < 5; i++) {
         for(int j = 0; j < 5; j++) {
-            std::cout << std::setw(7) << pairNumber << std::setw(10) << majorColor[i] << std::setw(10) << minorColor[j] << "\n";
+            std::cout << std::setw(12) << pairNumber << std::setw(10) << majorColor[i] << std::setw(10) << minorColor[j] << "\n";
             ++pairNumber;
         }
     }
